19d81a32fc032f2fa6f929bfbfcd852e2b221b9b
Albert Ramos <albertramos.dev@gmail.com>
2022-03-31 14:42:05 -0700

    Add new link to navigation bar

--- section_a/c02/includes/header.php
+++ section_a/c02/includes/header.php
@@ -9,6 +9,7 @@
     <nav>
       <a href="index.php">Home</a> | 
       <a href="candy.php">Candy</a> | 
+      <a href="specialties.php">Specialties</a> |
       <a href="about.php">About</a> | 
       <a href="contact.php">Contact</a>
     </nav>
